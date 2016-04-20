24188bd82f57637a45124d002318037da2c66338
naila <23naila.n@gmail.com>
2016-04-20 00:17:44 -0400

    fixed the semantic analysis stuff and changed the port so it does not conflict with the chindow's port

--- app/common/components/sim/simpreview.component.ts
+++ app/common/components/sim/simpreview.component.ts
@@ -25,56 +25,51 @@ export class SimPreview {
   }
 
   getMetrics(type) {
-  //   this.spinner.spin(document.getElementById('metrics-loading'));
+    this.spinner.spin(document.getElementById('metrics-loading'));
 
-  //   if (this.messenger.demoMessages.length == 0)
-  //     return false;
-  //   var texts = _.chain(this.messenger.demoMessages)
-  //       .filter((m) => m.author != 'hotel')
-  //       .map((m)=> m.body)
-  //       .value()
-  //       .join(' ')
+    if (this.messenger.receivedMessages.length == 0)
+      return false;
 
-  //   fetch(`https://docker.default/tone`, {
-  //     method: 'POST',
-  //     body: JSON.stringify({ "text": texts })
-  //   })
-  //     .then((res) => res.json())
-  //     .then((data) => {
+    fetch(`https://docker.default/tone`, {
+      method: 'POST',
+      body: JSON.stringify({ "text": this.messenger.receivedMessages })
+    })
+      .then((res) => res.json())
+      .then((data) => {
 
-  //       var graphData = {
-  //         labels: [],
-  //         datasets: [
-  //           {
-  //             fillColor: "rgba(220,220,220,0.2)",
-  //             strokeColor: "rgba(220,220,220,1)",
-  //             pointColor: "rgba(220,220,220,1)",
-  //             pointStrokeColor: "#fff",
-  //             pointHighlightFill: "#fff",
-  //             pointHighlightStroke: "rgba(220,220,220,1)",
-  //             data: []
-  //           }
-  //         ]
-  //       };
+        var graphData = {
+          labels: [],
+          datasets: [
+            {
+              fillColor: "rgba(220,220,220,0.2)",
+              strokeColor: "rgba(220,220,220,1)",
+              pointColor: "rgba(220,220,220,1)",
+              pointStrokeColor: "#fff",
+              pointHighlightFill: "#fff",
+              pointHighlightStroke: "rgba(220,220,220,1)",
+              data: []
+            }
+          ]
+        };
 
-  //       var p = JSON.parse(data.text);
-  //       console.log(p);
-  //       let tones = p.document_tone.tone_categories[type].tones;
-  //       tones.forEach((toneData) => {
-  //         graphData.labels.push(toneData.tone_name);
-  //         graphData.datasets[0].data.push(toneData.score);
-  //       })
-  //       this.spinner.stop()
-  //       var canvas = <HTMLCanvasElement> document.getElementById("myChart");
-  //       var ctx = canvas.getContext("2d");
-  //       ctx.clearRect(0, 0, canvas.width, canvas.height);
-  //       var myNewChart = new Chart(ctx).Bar(graphData, {
-  //         legendTemplate: "peter"
-  //       });
-  //     })
-  //     .catch(function(err) {
-  //       console.log(err);
-  //     });
+        var p = JSON.parse(data.text);
+        console.log(p);
+        let tones = p.document_tone.tone_categories[type].tones;
+        tones.forEach((toneData) => {
+          graphData.labels.push(toneData.tone_name);
+          graphData.datasets[0].data.push(toneData.score);
+        })
+        this.spinner.stop()
+        var canvas = <HTMLCanvasElement> document.getElementById("myChart");
+        var ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        var myNewChart = new Chart(ctx).Bar(graphData, {
+          legendTemplate: "peter"
+        });
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
   }
 
 }
