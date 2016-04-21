606fd65808ffea8214f625283abef427c03fb875
naila <23naila.n@gmail.com>
2016-04-21 16:19:27 -0400

    the green button on the settings page now routes to messaging

--- app/pages/dashboard/settings/settings.ts
+++ app/pages/dashboard/settings/settings.ts
@@ -2,6 +2,8 @@
 import {Component} from 'angular2/core';
 import {User} from '../../../common/models/User'
 import {Auth} from '../../../common/services/Auth'
+import {RouteParams, Router} from 'angular2/router';
+
 const template = require('./settings.jade');
 declare var require: any;
 
@@ -10,5 +12,9 @@ declare var require: any;
   template: template
 })
 export class SettingsComponent {
-  constructor() {}
+  constructor(private _router: Router) {}
+  
+  gotoMessaging() {
+    this._router.navigate(['Messaging']);
+  }
 }
