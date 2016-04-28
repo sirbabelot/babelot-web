d461b4d1b10896f43554d6c408860c7284067f37
naila <23naila.n@gmail.com>
2016-04-27 23:51:57 -0400

    packa

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 80;
 
 var app = express();
 app.use('/', express.static(__dirname + '/build'));
