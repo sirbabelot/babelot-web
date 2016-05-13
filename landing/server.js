30e35bbbe00e8076ded3feb364c8c162a9ff80aa
Dylan Harness <dharness@uwo.ca>
2016-05-13 01:10:28 -0400

    added live reload for dev env;

--- landing/server.js
+++ landing/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 const PORT = process.env.PORT || 8001;
 
 var app = express();
-app.use('/', express.static(__dirname));
+app.use('/', express.static(`${__dirname}/app`));
 
 app.listen(PORT, ()=> {
   console.log(`Listening on ${PORT}`);
