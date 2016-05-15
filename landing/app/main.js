02970fd15cfd25d52130428bc37ddeb479d53a04
Dylan Harness <dharness@uwo.ca>
2016-05-15 12:44:19 -0400

    toggle between windows

--- landing/app/main.js
+++ landing/app/main.js
@@ -1,6 +1,14 @@
-messenger.init();
-
+// messenger.init();
 
 function go(state) {
-  alert(state);
+  if (state == 'about') {
+    document.querySelector('.bablot-contact').style.display = 'none';
+    document.querySelector('.bablot-about').style.display = 'inline-block';
+  } else if (state == 'contact') {
+    document.querySelector('.bablot-contact').style.display = 'inline-block';
+    document.querySelector('.bablot-about').style.display = 'none';
+  } else if (state == 'chat') {
+    document.querySelector('.bablot-contact').style.display = 'none';
+    document.querySelector('.bablot-about').style.display = 'none';
+  }
 }
