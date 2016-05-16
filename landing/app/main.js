c2bfaa3d3771190418f2ce380cf32d0945001721
Dylan Harness <dharness@uwo.ca>
2016-05-15 21:55:58 -0400

    fixed positioning and movement

--- landing/app/main.js
+++ landing/app/main.js
@@ -1,14 +1,26 @@
 // messenger.init();
 
 function go(state) {
+  var chat = document.querySelector('.bablot-chat');
+  var about = document.querySelector('.bablot-about');
+  var wrap = document.querySelector('.wrapper');
+  var contact = document.querySelector('.bablot-contact');
+
   if (state == 'about') {
-    document.querySelector('.bablot-contact').style.display = 'none';
-    document.querySelector('.bablot-about').style.display = 'inline-block';
+    chat.style.animation = "slide-left 2s forwards"
+    about.style.display = 'block';
+    about.style.opacity = '0.5';
+    contact.style.opacity = '0';
+
   } else if (state == 'contact') {
-    document.querySelector('.bablot-contact').style.display = 'inline-block';
-    document.querySelector('.bablot-about').style.display = 'none';
+    chat.style.animation = "slide-left 2s forwards"
+    contact.style.display = 'block';
+    contact.style.opacity = '0.5';
+    about.style.opacity = '0';
+
   } else if (state == 'chat') {
-    document.querySelector('.bablot-contact').style.display = 'none';
-    document.querySelector('.bablot-about').style.display = 'none';
+    chat.style.animation = "slide-right 2s forwards"
+    about.style.opacity = '0';
+    contact.style.opacity = '0';
   }
 }
