4cc606420046509fd8700b8ac8d135d79c375979
Dylan Harness <dharness@uwo.ca>
2016-05-16 01:11:55 -0400

    added styling of chat window

--- landing/app/main.js
+++ landing/app/main.js
@@ -1,30 +1,41 @@
 // messenger.init();
 
+window.onload = function () {
+  var messages = document.querySelector('.bablot-chat--messages');
+  for(let i = 0; i <=50; i++) {
+    var e = document.createElement('div');
+    e.innerHTML = `<img src="assets/king3.png"/>
+      <div class="bablot-chat--message--text">I am a blab</div>`;
+    e.className += 'bablot-chat--message--received';
+    messages.appendChild(e);
+  }
+
+};
 function go(state) {
-  var chat = document.querySelector('.bablot-chat');
-  var about = document.querySelector('.bablot-about');
-  var contact = document.querySelector('.bablot-contact');
+  var chatPanel = document.querySelector('.bablot-chat-panel');
+  var aboutPanel = document.querySelector('.bablot-about-panel');
+  var contactPanel = document.querySelector('.bablot-contact-panel');
   var wrap = document.querySelector('.bablot-wrapper');
 
   if (state == 'about') {
-    chat.style.animation = "slide-left 1s forwards"
-    about.style.display = 'block';
-    about.style.opacity = '0.5';
-    contact.style.opacity = '0';
+    chatPanel.style.animation = "slide-left 1s forwards"
+    aboutPanel.style.display = 'block';
+    aboutPanel.style.opacity = '1';
+    contactPanel.style.opacity = '0';
 
   } else if (state == 'contact') {
-    chat.style.animation = "slide-left 1s forwards"
-    contact.style.display = 'block';
-    contact.style.opacity = '0.5';
-    about.style.opacity = '0';
+    chatPanel.style.animation = "slide-left 1s forwards"
+    contactPanel.style.display = 'block';
+    contactPanel.style.opacity = '1';
+    aboutPanel.style.opacity = '0';
 
   } else if (state == 'chat') {
     // Needed to prevent the animation from firing
     // if nothing has been clicked yet
-    if (chat.style.animation !== '') {
-      chat.style.animation = "slide-right 1s forwards"
+    if (chatPanel.style.animation !== '') {
+      chatPanel.style.animation = "slide-right 1s forwards"
     }
-    about.style.opacity = '0';
-    contact.style.opacity = '0';
+    aboutPanel.style.opacity = '0';
+    contactPanel.style.opacity = '0';
   }
 }
