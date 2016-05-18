09e84a35ccbfa31e4bc0064f420b6daf6f865133
naila <23naila.n@gmail.com>
2016-05-18 17:29:46 -0400

    combined prod and dev servers

--- landing/server.js
+++ landing/server.js
@@ -1,9 +1,26 @@
 var express = require('express');
+var WebpackDevServer = require("webpack-dev-server");
+var webpack = require("webpack");
+var config = require('./webpack.config.js');
 const PORT = process.env.PORT || 8001;
 
-var app = express();
-app.use('/', express.static(`${__dirname}/app`));
 
-app.listen(PORT, ()=> {
-  console.log(`Listening on ${PORT}`);
+if (process.env.NODE_ENV === 'development') {
+
+  var compiler = webpack(config);
+  var server = new WebpackDevServer(compiler, {
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000
+    }
+  }); 
+
+} else if (process.env.NODE_ENV === 'production') {
+
+  var server = express();
+  server.use('/', express.static(`${__dirname}/build`));
+}
+
+server.listen(PORT, "0.0.0.0", ()=> {
+    console.log(`ENV=${process.env.NODE_ENV} - Listening on ${PORT}`);
 });
